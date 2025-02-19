e5e037864a2673d44a0fbe9a93b6fecdd8ef1b6f
cjluther26 <cjluther26@yahoo.com>
2025-02-19 17:53:37 -0500

    adding functionality to get fhv dadta

--- 03-data-warehouse/homework/get_taxi_data.py
+++ 03-data-warehouse/homework/get_taxi_data.py
@@ -3,6 +3,7 @@ import urllib.request
 from concurrent.futures import ThreadPoolExecutor
 from google.cloud import storage
 import time
+import pandas as pd
 
 
 #Change this to your bucket name
@@ -18,8 +19,9 @@ else:
     client = storage.Client()
 
 
-BASE_URL = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-"
-MONTHS = [f"{i:02d}" for i in range(1, 7)] 
+# BASE_URL = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-"
+BASE_URL = "https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_2019-"
+MONTHS = [f"{i:02d}" for i in range(1, 13)] 
 DOWNLOAD_DIR = "."
 
 CHUNK_SIZE = 8 * 1024 * 1024  
@@ -29,15 +31,44 @@ os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 bucket = client.bucket(BUCKET_NAME)
 
 
+def process_parquet(file_path):
+    print(f"Processing {file_path}...")
+
+    # Read the Parquet file
+    df = pd.read_parquet(file_path)
+
+    # Convert ['SR_Flag', 'PUlocationID', 'DOlocationID'] to INT64 if it exists
+    for col in ['SR_Flag', 'PUlocationID', 'DOlocationID']:
+        if col in df.columns:
+            df[col] = pd.to_numeric(df[col], errors='coerce').fillna(0).astype('int64')
+            print(f"'{col}' column converted to INT64.")
+        else:
+            print(f"'{col}' column not found. Skipping conversion.")
+   
+    print(f"Converted columns...here are the first few rows:\n{df.head()}")
+
+    # Save the modified Parquet file (overwrite original)
+    df.to_parquet(file_path, index=False)
+
+    print(f"Saved updated file: {file_path}")
+
+    return file_path
+
+
+
 def download_file(month):
     url = f"{BASE_URL}{month}.parquet"
-    file_path = os.path.join(DOWNLOAD_DIR, f"yellow_tripdata_2024-{month}.parquet")
+    # file_path = os.path.join(DOWNLOAD_DIR, f"yellow_tripdata_2024-{month}.parquet")
+    file_path = os.path.join(DOWNLOAD_DIR, f"fhv_tripdata_2019-{month}.parquet")
 
     try:
         print(f"Downloading {url}...")
         urllib.request.urlretrieve(url, file_path)
         print(f"Downloaded: {file_path}")
-        return file_path
+        processed_file_path = process_parquet(file_path)
+        print(f"Converted: {file_path}")
+        return processed_file_path
+    
     except Exception as e:
         print(f"Failed to download {url}: {e}")
         return None
